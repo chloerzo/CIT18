e778f31de2bb6b19f37aa65649f9976678ef19d4
chloerzo <153843882+chloerzo@users.noreply.github.com>
2025-02-12 04:44:18 +0000

    Created migration up to controllers

---
+++ laravel1/app/Http/Controllers/TaskController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Task::all());
+    }
+
+    /**
+     * Show a specific task.
+     */
+    public function show(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'is_completed' => 'boolean'
+        ]);
+    }
+
+    
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

55e6c6ed25e6e3f12c736f9b8169217073fa1601
chloerzo <153843882+chloerzo@users.noreply.github.com>
2025-03-05 11:42:30 +0000

    Installed Tailwindcss. Server not working

--- laravel1/resources/views/greet.blade.php
+++ laravel1/resources/views/greet.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <title>Greeting</title>
 </head>
 <body>
-    <h1>This is the greet view!</h1>
+    <h1 class="text-3xl font-bold underline">This is the greet view!</h1>
 </body>
 </html>
